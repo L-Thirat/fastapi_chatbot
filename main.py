6de47a2942c574fe419f777facbbcf1572d7a9ee
L-thirat <terus_limsurut@hotmail.com>
2024-03-07 01:14:55 +0900

    add api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, AutoModelForSeq2SeqLM, pipeline
 from Conversation.conversation import character_msg_constructor
 import torch
 
@@ -13,12 +13,16 @@ if use_gpu:
 # ---------- load Conversation model ----------
 print("Initilizing model....")
 print("Loading language model...")
-tokenizer = AutoTokenizer.from_pretrained("sambanovasystems/SambaLingo-Thai-Chat", use_fast=True)
-config = AutoConfig.from_pretrained("sambanovasystems/SambaLingo-Thai-Chat", is_decoder=True, torch_dtype=torch.float16)
-model = AutoModelForCausalLM.from_pretrained("sambanovasystems/SambaLingo-Thai-Chat", config=config, )
 
-if use_gpu:  # load model to GPU
-    model = model.to(device)
+pipe = pipeline("text-generation", model="sambanovasystems/SambaLingo-Thai-Chat", device_map="auto", use_fast=False)
+
+
+# tokenizer = AutoTokenizer.from_pretrained("sambanovasystems/SambaLingo-Thai-Chat", use_fast=False)
+# config = AutoConfig.from_pretrained("sambanovasystems/SambaLingo-Thai-Chat", is_decoder=True, torch_dtype=torch.float16)
+# model = AutoModelForCausalLM.from_pretrained("sambanovasystems/SambaLingo-Thai-Chat", config=config, ).half()
+#
+# if use_gpu:  # load model to GPU
+#     model = model.to(device)
 
 print('--------Finished!----------')
 # --------------------------------------------------
@@ -79,85 +83,91 @@ async def get_waifuapi(command: str, data: str):
         # ----------- Create Response --------------------------
         msg = talk.construct_msg(msg, talk.history_loop_cache)  # construct message input and cache History model
         ## ----------- Will move this to server later -------- (16GB ram needed at least)
-        inputs = tokenizer(msg, return_tensors='pt')
-        if use_gpu:
-            inputs = inputs.to(device)
-        print("generate output ..\n")
-        out = model.generate(**inputs, max_length=len(inputs['input_ids'][0]) + 80,  # todo 200 ?
-                             pad_token_id=tokenizer.eos_token_id)
-        conversation = tokenizer.decode(out[0])
-        print("conversation .. \n" + conversation)
+        messages = [
+            {"role": "user", "content": {msg}},
+        ]
+        prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True, max_length=200)
+        outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)[0]
+        conversation = outputs["generated_text"]
+        # inputs = tokenizer(msg, return_tensors='pt')
+        # if use_gpu:
+        #     inputs = inputs.to(device)
+        # print("generate output ..\n")
+        # out = model.generate(**inputs, max_length=len(inputs['input_ids'][0]) + 80,  # todo 200 ?
+        #                      pad_token_id=tokenizer.eos_token_id)
+        # conversation = tokenizer.decode(out[0])
+        # print("conversation .. \n" + conversation)
+        #
+        # ## --------------------------------------------------
+        #
+        # ## get conversation in proper format and create history from [last_idx: last_idx+2] conversation
+        # # talk.split_counter += 0
+        # print("get_current_converse ..\n")
+        # current_converse = talk.get_current_converse(conversation)
+        # print("answer ..\n")  # only print waifu answer since input already show
+        # print(current_converse)
+        # # talk.history_loop_cache = '\n'.join(current_converse)  # update history for next input message
+        #
+        # # -------------- use machine translation model to translate to japanese and submit to client --------------
+        # print("cleaning ..\n")
+        # cleaned_text = talk.clean_emotion_action_text_for_speech(current_converse[1])  # clean text for speech
+        # cleaned_text = cleaned_text.split("Lilia: ")[-1]
+        # cleaned_text = cleaned_text.replace("<USER>", "Fuse-kun")
+        # cleaned_text = cleaned_text.replace("\"", "")
+        # if cleaned_text:
+        #     print("cleaned_text\n" + cleaned_text)
+        #
+        #     txt = cleaned_text  # initialize translated text as empty by default
+        #
+        #     # ----------- Waifu Expressing ----------------------- (emotion expressed)
+        #     emotion = talk.emotion_analyze(current_converse[1])  # get emotion from waifu answer (last line)
+        #     print(f'Emotion Log: {emotion}')
+        #     emotion_to_express = 'netural'
+        #     if 'joy' in emotion:
+        #         emotion_to_express = 'happy'
+        #
+        #     elif 'anger' in emotion:
+        #         emotion_to_express = 'angry'
+        #
+        #     print(f'Emotion to express: {emotion_to_express}')
 
-        ## --------------------------------------------------
-
-        ## get conversation in proper format and create history from [last_idx: last_idx+2] conversation
-        # talk.split_counter += 0
-        print("get_current_converse ..\n")
-        current_converse = talk.get_current_converse(conversation)
-        print("answer ..\n")  # only print waifu answer since input already show
-        print(current_converse)
-        # talk.history_loop_cache = '\n'.join(current_converse)  # update history for next input message
-
-        # -------------- use machine translation model to translate to japanese and submit to client --------------
-        print("cleaning ..\n")
-        cleaned_text = talk.clean_emotion_action_text_for_speech(current_converse[1])  # clean text for speech
-        cleaned_text = cleaned_text.split("Lilia: ")[-1]
-        cleaned_text = cleaned_text.replace("<USER>", "Fuse-kun")
-        cleaned_text = cleaned_text.replace("\"", "")
-        if cleaned_text:
-            print("cleaned_text\n" + cleaned_text)
-
-            txt = cleaned_text  # initialize translated text as empty by default
-
-            # ----------- Waifu Expressing ----------------------- (emotion expressed)
-            emotion = talk.emotion_analyze(current_converse[1])  # get emotion from waifu answer (last line)
-            print(f'Emotion Log: {emotion}')
-            emotion_to_express = 'netural'
-            if 'joy' in emotion:
-                emotion_to_express = 'happy'
-
-            elif 'anger' in emotion:
-                emotion_to_express = 'angry'
-
-            print(f'Emotion to express: {emotion_to_express}')
-
-            return JSONResponse(content=f'{emotion_to_express}<split_token>{txt}')
-        else:
-            return JSONResponse(content=f'NONE<split_token> ')
-    elif command == "story":
-        msg = data
-        # ----------- Create Response --------------------------
-        msg = talk.construct_msg(msg, talk.history_loop_cache)  # construct message input and cache History model
-        ## ----------- Will move this to server later -------- (16GB ram needed at least)
-        inputs = tokenizer(msg, return_tensors='pt')
-        if use_gpu:
-            inputs = inputs.to(device)
-        out = model.generate(**inputs, max_length=len(inputs['input_ids'][0]) + 100,
-                             pad_token_id=tokenizer.eos_token_id)
-        conversation = tokenizer.decode(out[0])
-        print("conversation" + conversation)
-
-        ## --------------------------------------------------
-
-        ## get conversation in proper format and create history from [last_idx: last_idx+2] conversation
-        talk.split_counter += 0
-        current_converse = talk.get_current_converse(conversation)[:talk.split_counter][
-                           talk.split_counter - 2:talk.split_counter]
-        print("answer" + conversation)  # only print waifu answer since input already show
-        talk.history_loop_cache = '\n'.join(current_converse)  # update history for next input message
-
-        # -------------- use machine translation model to translate to japanese and submit to client --------------
-        cleaned_text = talk.clean_emotion_action_text_for_speech(current_converse[-1])  # clean text for speech
-
-        translated = ''  # initialize translated text as empty by default
-
-        return JSONResponse(content=f'{current_converse[-1]}<split_token>{translated}')
-
-    if command == "reset":
-        talk.conversation_history = ''
-        talk.history_loop_cache = ''
-        talk.split_counter = 0
-        return JSONResponse(content='Story reseted...')
+        return JSONResponse(content=conversation)
+        # else:
+        #     return JSONResponse(content=f'NONE<split_token> ')
+    # elif command == "story":
+    #     msg = data
+    #     # ----------- Create Response --------------------------
+    #     msg = talk.construct_msg(msg, talk.history_loop_cache)  # construct message input and cache History model
+    #     ## ----------- Will move this to server later -------- (16GB ram needed at least)
+    #     inputs = tokenizer(msg, return_tensors='pt')
+    #     if use_gpu:
+    #         inputs = inputs.to(device)
+    #     out = model.generate(**inputs, max_length=len(inputs['input_ids'][0]) + 100,
+    #                          pad_token_id=tokenizer.eos_token_id)
+    #     conversation = tokenizer.decode(out[0])
+    #     print("conversation" + conversation)
+    #
+    #     ## --------------------------------------------------
+    #
+    #     ## get conversation in proper format and create history from [last_idx: last_idx+2] conversation
+    #     talk.split_counter += 0
+    #     current_converse = talk.get_current_converse(conversation)[:talk.split_counter][
+    #                        talk.split_counter - 2:talk.split_counter]
+    #     print("answer" + conversation)  # only print waifu answer since input already show
+    #     talk.history_loop_cache = '\n'.join(current_converse)  # update history for next input message
+    #
+    #     # -------------- use machine translation model to translate to japanese and submit to client --------------
+    #     cleaned_text = talk.clean_emotion_action_text_for_speech(current_converse[-1])  # clean text for speech
+    #
+    #     translated = ''  # initialize translated text as empty by default
+    #
+    #     return JSONResponse(content=f'{current_converse[-1]}<split_token>{translated}')
+    #
+    # if command == "reset":
+    #     talk.conversation_history = ''
+    #     talk.history_loop_cache = ''
+    #     talk.split_counter = 0
+    #     return JSONResponse(content='Story reseted...')
 
 
 if __name__ == "__main__":
